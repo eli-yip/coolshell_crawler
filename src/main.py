ab529d18b0300477ad79c9800e5eb4cffc70cc0a
Eli Yip <yezi.2022@outlook.com>
2023-05-15 22:09:36 +0800

    feat: main.py get all urls

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_data(page_url: str):
+
+    response = requests.get(page_url, timeout=1000)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    h2_tags = soup.find_all('h2', {'class': 'entry-title'})
+    links_result = [tag.find('a')['href'] for tag in h2_tags]
+
+    return links_result
+
+
+def write_to_file(artical_urls: list):
+    with open('links.txt', 'a', encoding='utf-8') as file:
+        for link in artical_urls:
+            file.write(f'{link}\n')
+
+
+if __name__ == '__main__':
+    url = 'https://coolshell.cn/'
+    # links = get_data(url)
+    # print("Get data from page: ", url)
+    # write_to_file(links)
+    for page_num in range(35, 74):
+        url = f'https://coolshell.cn/page/{page_num}'
+        links = get_data(url)
+        print("Get data from page: ", url)
+        write_to_file(links)
