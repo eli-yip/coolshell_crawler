2708cef0164fc6069eba598b399ef5d8ac1bbdbf
Eli Yip <yezi.2022@outlook.com>
2023-05-15 22:28:36 +0800

    fix: web archive support

--- src/get_content.py
+++ src/get_content.py
@@ -9,33 +9,43 @@ with open('links.txt', 'r', encoding='utf-8') as file:
 
 target_directory = os.path.join(os.getcwd(), 'articles')
 
+if not os.path.exists(target_directory):
+    os.mkdir(target_directory)
 
 # 对每个URL进行处理
 for url in urls:
+    article_id = 'post-' + \
+        url.split('/')[-1].split('.')[0]  # 从URL获取文章ID
+    url = 'https://web.archive.org/web/' + url
     attempts = 0
     max_attempts = 100  # 设定最大尝试次数，防止无限循环
     while attempts < max_attempts:
         try:
             # 发送请求获取网页内容
-            response = requests.get(url, timeout=1000)
-            # 使用BeautifulSoup解析HTML
-            soup = BeautifulSoup(response.text, 'html.parser')
-            # 使用文章的ID查找article元素
-            article_id = 'post-' + \
-                url.split('/')[-1].split('.')[0]  # 从URL获取文章ID
-            article = soup.find('article', {'id': article_id})
+            response = requests.get(url, timeout=1000, allow_redirects=True)
 
-            if article is not None:
+            # 确保重定向完成后再获取内容
+            if response.history:
+                final_url = response.url
+                print("最终URL:", final_url)
+                content = response.content
+                # 处理获取到的内容
+            else:
+                print("无重定向")
+
+            text = content.decode('utf-8')
+
+            if text is not None:
                 # 获取文章内容
-                article_content = article.text
+                article_content = text
                 target_file = os.path.join(
-                    target_directory, f'{article_id}.txt')
+                    target_directory, f'{article_id}.html')
 
                 # 创建一个文件来保存文章内容
                 with open(target_file, 'w', encoding='utf-8') as file:
                     file.write(article_content)
 
-                print(f'Successfully saved: {article_id}.txt')
+                print(f'Successfully saved: {article_id}.html')
                 break  # 成功获取到文章内容，跳出循环
             print('Article not found for URL:', url)
             attempts += 1
